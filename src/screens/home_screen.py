3558d168544dc0a699c80cc3b53485fd0e9bcab8
ShivGak05 <160029224+ShivGak05@users.noreply.github.com>
2026-05-31 23:59:28 +0530

    Remove header from home screen

--- src/screens/home_screen.py
+++ src/screens/home_screen.py
@@ -4,7 +4,6 @@ from src.ui.base_layout import style_base_layout, style_background_home
 from src.components.footer import footer_home
 
 def home_screen():
-    st.header("Home Screen")
     header_home()
     style_background_home()
     style_base_layout()
