a305120d0e4b2ad20aa6e36eb27e2acae06003c0
ShivGak05 <160029224+ShivGak05@users.noreply.github.com>
2026-06-01 00:09:22 +0530

    Update app domain in share_subject_dialogue.py

--- src/components/share_subject_dialogue.py
+++ src/components/share_subject_dialogue.py
@@ -5,7 +5,7 @@ import io
 
 @st.dialog("Share class link")
 def share_subject_dialogue(subject_name,subject_code):
-    app_domain="http://localhost:8501"
+    app_domain="snapclass-main-shivgak"
     join_url=f"{app_domain}/?join-code={subject_code}"
     st.header("Scan to Join")
     qr=segno.make(join_url)
